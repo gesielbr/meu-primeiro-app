f32c9b0141fa7b9125c8f55c55ccf9bd3ca5a9f5
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-06-23 21:16:40 -0300

    NgDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `
+    <app-title *ngIf="destruir"></app-title><br /><br />
+    <button (click)="destruirComponent()">Destruir componente</button>
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent {
-  title = 'meu-primeiro-app';
+export class AppComponent implements OnInit {
+  public destruir: boolean = true;
+  constructor() {}
+  ngOnInit(): void {}
+
+  public destruirComponent() {
+    this.destruir = !this.destruir;
+  }
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit {
-  public title: string = 'Bem vindo';
+export class TitleComponent implements OnInit, OnDestroy {
+  @Input() public qqrvariavel: string = 'Bem vindo';
 
   constructor() {}
-
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    alert('Component destruído!');
+  }
 }
