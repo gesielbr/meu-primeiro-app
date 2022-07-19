0870e9bea9a7397e14fe23b4fc67506d6995da6f
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-07-19 11:04:40 -0300

    NgClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-data-binding></app-data-binding>
+    <!-- <app-data-binding></app-data-binding>
+    <app-diretivas-estruturais></app-diretivas-estruturais> -->
+    <app-diretivas-atributos></app-diretivas-atributos>
     <router-outlet></router-outlet>
-    <app-diretivas-estruturais></app-diretivas-estruturais>
   `,
 })
 export class AppComponent implements OnInit {
