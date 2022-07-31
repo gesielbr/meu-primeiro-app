d1049a2868f0c80baa801e0b4133f1074f6a3622
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-07-30 21:19:41 -0300

    @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,18 @@ import { Component, OnInit } from '@angular/core';
       <h1>Aula de Diretivas de Atributo</h1>
       <hr />
     </app-diretivas-atributos> -->
-    <app-new-component></app-new-component>
+    <!-- <app-new-component></app-new-component> -->
+    <app-input [contador]="addValue"></app-input><br />
+    <button (click)="add()">AddValue</button>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
+  public addValue: number = 0;
   constructor() {}
   ngOnInit(): void {}
+
+  public add() {
+    this.addValue += 1;
+  }
 }
